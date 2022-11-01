bd4dd960e890faace1a944e548de9a91da2dcf9d
ZzZzZzu <zcahjfa@ucl.ac.uk>
2022-11-01 21:15:59 +0000

    add some information to my group

--- group.py
+++ group.py
@@ -1,5 +1,39 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+my_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
 
-my_group =
+
+print (f"Nash is {my_group['Nash']['age']}")
+print(",".join(my_group.keys()))
+print(f"{my_group['Jill']['relations']}")
