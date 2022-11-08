e4f27cca6fb882e7db41be5f3fc41d827d60ffc3
ZzZzZzu <zcahjfa@ucl.ac.uk>
2022-11-08 11:59:27 +0000

    Doing Homework

--- group.py
+++ group.py
@@ -40,11 +40,11 @@ print(f"{my_group['Jill']['relations']}")
 
 
 
-def average_age()
+def average_age():
     age = 0
-     for person in my_group:
+    for person in my_group:
          age += person["age"]
-     return age / len(my_group)
+    return age / len(my_group)
 
 def add_person(name, age, job, relations):
      my_group.append({"name":name, "age":age, "job":job, "relations":relations})
@@ -54,3 +54,16 @@ def forget(person1, person2):
      my_group[person1]["relations"][person2] = ""
      my_group[person2]["relations"][person1] = ""
      return my_group
+
+
+
+
+# Homework
+def mean(data):
+    return sum(data) / len(data)
+
+
+print(max(person["age"] for person in my_group.values()))  # 34
+print(mean([len(person["relations"]) for person in my_group.values()]))  # 1.5
+print(max(person["age"] for person in my_group.values() if person["relations"]))  # 34
+print(max(person["age"] for person in my_group.values() if "friend" in person["relations"].values()))  # 28
